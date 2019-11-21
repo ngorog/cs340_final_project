f9b11ae8f6577b536b43376eafca780e4b4e1259
dongrogers <dongrog@oregonstate.edu>
2019-11-20 16:41:45 -0800

    Working on editing menu

---
+++ javascript/AJAX/editMenuItem.php
@@ -0,0 +1,24 @@
+<?php
+	include '../../connectdb.php';
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	session_start();
+
+	if(isset($_POST['id'] && isset($_POST['price']) && isset($_POST['name']) && isset($_POST['desc']))){
+		$pid = $conn->real_escape_string($_POST['id']);
+		$name = $conn->real_escape_string($_POST['name']);
+		$price = $conn->real_escape_string($_POST['price']);
+		$desc = $conn->real_escape_string($_POST['desc']);
+		
+		$sql = "UPDATE Product
+					SET ProductName   ='".$name."',
+						Price         ='".$price."',
+						Description   ='".$desc."'
+					WHERE
+						ProductId = '". $pid . "'";
+		$conn->query($sql);
+		echo 1;
+	}
+	else{
+		echo 0;
+	}
+?>
--- javascript/menu.js
+++ javascript/menu.js
@@ -1,31 +1,56 @@
 $(document).ready(function(){
 	//Edit Personal Info
 	$(".editMenu").click(function() {
-		console.log($(this));
 		editMenu($(this));
 	});
-//	$("#pInfoTable").on("keypress", ".editP", function(e){
-//		if(e.key == "Enter")
-//			confirmPEdits();
-//	})
-//	$("#confirmPInfo").on("click", confirmPEdits);
 //	$("#cancelPInfo").on("click", cancelPEdits);
 });
 
 
 function editMenu(obj){
 	var pid = $(obj).attr('value');
+	$(".editMenu[value="+pid+"]").addClass("d-none");
+	$("#saveCancel"+pid).removeClass("d-none");
 	$.getJSON("javascript/AJAX/getMenuItemInfo.php", {ProductId: pid}, function(data){
-		console.log(data);
-		$("#name"+pid).html("<input class='form-control d-inline' type='text' id='editName'"+pid+" placeholder='"+data.ProductName+"'></input>");
-		$("#price"+pid).html("<input class='form-control d-inline' type='text' id='editPrice'"+pid+" placeholder='$"+data.Price+"'></input>");
+		$("#collapser").attr('href', 'none'); //Disabled collapse
+		$("#menuItem"+pid).replaceWith($('<form id='+pid+' class="my-3 p-3 bg-white rounded shadow-sm">' + $("#menuItem"+pid)[0].innerHTML + '</form>')); //Change to form for submit
+		$("#namePrice"+pid).html(
+			"<div class='row'>"+
+				"<div class='col'>"+
+					"<input id='editName' type='text' class='form-control' minlength='2' maxlength='45' value='"+data.ProductName+"'>"+
+				"</div>"+
+				"<div class='col'>"+
+					"<input id='editPrice' type='number' min='0.00' max='1000.00' step='0.01' class='form-control' value='"+data.Price+"'>"+
+				"</div>"+
+			"</div>");
+		$("#description"+pid).html("<textarea class='form-control' minlength='10' maxlength='500' id='editDescription'>"+data.Description+"</textarea>");
+		//Add event listener to new form for submittal
+		$("form").on("submit", function(){
+			confirmMenu($(this));		
+		});
 	});
-
-
 }
 
-function confirmMenu(){
+function validatePrice(price){
+}
 
+function confirmMenu(obj){
+	var pid = obj.attr('id');
+	var newPrice = obj.find("#editPrice").val();
+	var newName = obj.find("#editName").val();
+	var newDesc = obj.find("#editDescription").val();
+
+	var ItemInfo = {
+		'id': pid,
+		'price': newPrice,
+		'name': newName,
+		'desc': newDesc
+	};
+	
+	$.post("javascript/AJAX/editMenuItem.php", ItemInfo, function(data){
+		console.log(ItemInfo);
+		console.log('Tried?' + data);
+	});
 }
 
 function cancelMenu(){
