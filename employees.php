95e3d6a251f8a5e192cc1179e174bff233819f85
Jie Li <liji2@oregonstate.edu>
2019-11-25 15:21:45 -0800

    Worked on employees

--- employees.php
+++ employees.php
@@ -1,7 +1,7 @@
 <?php
 	include 'connectdb.php';	
 	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	
+	session_start();
 	if(!$conn){
 		die("Unable to connect to database " . mysql_error());
 	}
@@ -13,12 +13,13 @@
         <title>Employee Information</title>
         <link rel="stylesheet" href="index.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script type='text/javascript' src="jquery-3.1.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
         <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
-
+        <script src="javascript/employees.js"></script>
     </head>
 
 
@@ -41,6 +42,8 @@
 						<th>Last Name</th>
 						<th>Position Name</th>
 						<th>Payrate</th>
+						<!--If Manager/Owner -->
+						<th></th>
 					</tr>
 			<?php
 				$sql = "SELECT *
@@ -50,13 +53,31 @@
 				$sql_get = $conn->query($sql);
 				while($row = $sql_get->fetch_assoc()){
 				?>
-					<tr>
-						<td><?= $row['FirstName']?></td>
-						<td><?= $row['LastName']?></td>
-						<td><?= $row['EmpCategory']?></td>
-						<td><?= "$".$row['Wage']?></td>
-					</tr>
+					<tr id ='Employee<?= $row['EmployeeId'] ?>'>
+						<td id='FirstName<?= $row['EmployeeId']?>'>
+							<?= $row['FirstName']?>
+						</td>
+	
+						<td id='LastName<?= $row['EmployeeId']?>'>
+							<?= $row['LastName']?>
+						</td>
 
+						<td id='EmpCat<?= $row['EmployeeId']?>'>
+							<?= $row['EmpCategory']?>
+						</td>
+
+						<td id='Wage<?= $row['EmployeeId']?>'>
+							<?= $row['Wage']; ?>
+						</td>
+					<!--If Manager/Owner -->
+						<td>
+							<button class='editEmployees btn btn-info btn-sm' type='button' value='<?= $row['EmployeeId'] ?>'>Edit</button>
+							<span id='saveCancel<?= $row['EmployeeId'] ?>' class='d-none'>						
+								<button class='editCancel btn btn-secondary btn-sm' type='button' value='<?= $row['EmployeeId'] ?>'>Cancel</button>
+								<button class='editSave btn btn-success btn-sm' type='submit' value='<?= $row['EmployeeId']?>'>Save</button>
+							</span>
+						</td>
+					</tr>
 				<?php
 				}
 				$sql_get->close();
