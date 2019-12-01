1fa03258ebb123dae2decccc1a08a7c7b384a2ae
Zhifeng Lu <luzhi@flip3.engr.oregonstate.edu>
2019-11-30 18:49:30 -0800

    delmenuphpbroken

---
+++ javascript/AJAX/delMenuItem.php
@@ -0,0 +1,13 @@
+<?php
+	include '../../connectdb.php';
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	session_start();
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(-1);
+	if(isset($_POST['del_btn']) and is_numeric($_POST['del_btn'])){
+		$del = $_POST['del_btn'];
+		$sql = "DELETE FROM Product WHERE ProductId = '$del'";
+		$conn->query($sql);
+	}
+?>
