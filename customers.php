ad7850657ccb33ddbb289ccf8c76ba1125f5b699
dongrogers <dongrog@oregonstate.edu>
2019-12-03 00:46:21 -0800

    Renamed title

--- customers.php
+++ customers.php
@@ -18,7 +18,7 @@
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8">
-        <title>Employee Information</title>
+        <title>Customer Information</title>
         <link rel="stylesheet" href="index.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
